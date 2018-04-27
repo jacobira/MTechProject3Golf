3738b516f85327e43268ba20e689751c1db87867
Jacob Baldwin <jacobbaldwin@Jacobs-MacBook-Pro.local>
2018-04-27 09:41:23 -0600

    Further updates to project to include in and out columns and proper totaling.

--- action.js
+++ action.js
@@ -8,10 +8,47 @@ createCard();
 loadPlayers();
 
 function createCard(){
-    for (let i = 0; i < golfHoles.length; i++){
-        $(".right").append("<div id='col" + (i + 1) +"' class='column'><div class='colHeader'>" + (i + 1) + "</div></div>");
+    for (let i = 0; i < 9; i++){
+        $(".right").append("<div id='col" + (i + 1) +"' class='column'>" +
+            "<div class='colHeader'>" +
+            "<div class='holeNum'>" + (i + 1) + "</div>" +
+            "<div class='parInfo' id='parInfo" + (i + 1) + "'>" +
+            "<div class='par' id='par-" + i + "'>PAR</div>" +
+            "<div class='yardage'>YDG:</div>" +
+            "</div>" +
+            "</div>" +
+            "</div>");
     }
-    $(".right").append("<div class='scoreCol'><div class='scoreColHead'>Total</div></div>");
+    $(".right").append("<div class='scoreCol' id='scoreColIn'>" +
+        "<div class='scoreColHead'>" +
+        "<div class='scoreColLabel'>In</div>" +
+        "<div class='scoreColParTotal'>TOTAL PAR:</div>" +
+        "</div>" +
+        "</div>");
+
+    for (let i = 9; i < 18; i++){
+        $(".right").append("<div id='col" + (i + 1) +"' class='column'>" +
+            "<div class='colHeader'>" +
+            "<div class='holeNum'>" + (i + 1) + "</div>" +
+            "<div class='parInfo' id='parInfo" + (i + 1) + "'>" +
+            "<div class='par' id='par-" + i + "'>PAR</div>" +
+            "<div class='yardage'>YDG:</div>" +
+            "</div>" +
+            "</div>" +
+            "</div>");
+    }
+    $(".right").append("<div class='scoreCol' id='scoreColOut'>" +
+        "<div class='scoreColHead'>" +
+        "<div class='scoreColLabel'>Out</div>" +
+        "<div class='scoreColParTotal'>TOTAL PAR:</div>" +
+        "</div>" +
+        "</div>");
+    $(".right").append("<div class='scoreCol' id='scoreColTotals'>" +
+        "<div class='scoreColHead'>" +
+        "<div class='scoreColLabel'>Totals</div>" +
+        "<div class='scoreColParTotal'>COURSE TOTAL PAR:</div>" +
+        "</div>" +
+        "</div>");
     console.log("Scorecard created")
 }
 
@@ -32,17 +69,18 @@ function loadPlayers(){
 
 function addScoreBoxes(player){
     for (let i = 0; i <= golfHoles.length; i++){
-        $("#col" + i).append('<div class="colScores" id="score'+ player +'-'+ i +'" contenteditable="true" onkeyup="scoreTotals('+ player +')">\n' +
-            '\n' +
-            '    0</div>');
+        $("#col" + i).append('<div class="colScores" id="score'+ player +'-'+ i +'" contenteditable="true" onkeyup="scoreTotals('+ player +')">' + 0 + '</div>');
     }
-    $(".scoreCol").append("<div class='total' id='total"+player+"'>0</div>");
+    $("#scoreColIn").append("<div class='total' id='inTotal"+player+"'>" + 0 + "</div>");
+    $("#scoreColOut").append("<div class='total' id='outTotal"+player+"'>" + 0 + "</div>");
+    $("#scoreColTotals").append("<div class='total' id='gameTotal"+player+"'>" + 0 + "</div>");
 }
 
 
 function addPlayer(){
     numPlayersUsed++;
     numPlayers++;
+
     $(".left").append('<div class="playerLabel" id="player'+ numPlayersUsed +'">\n' +
         '    <div class="playerName" contenteditable="true">\n' +
         '    Player '+ numPlayersUsed +'</div>\n' +
@@ -66,24 +104,26 @@ function deletePlayer(playerToTrash, playerNum){
 }
 
 function scoreTotals(player){
-    let initScore = 0;
-    let scCheck = [];
+    let inScore = 0;
+    let outScore = 0;
 
-    for (let i = 0; i < golfHoles.length; i++){
-        scCheck[i] = $("#score" + player + "-" + i + "")[0].innerHTML;
 
-        if(!isNaN(Number(scCheck[i])) === true){
-            initScore = scCheck[i] + initScore;
+    for (let i = 0; i < 9; i++){
+        let scoreInq = Number($("#score" + player + "-" + i + "").html());
+        if(isNaN(scoreInq) === false) {
+            inScore =  scoreInq + inScore;
+        }
+    }
+    for (let i = 9; i < 18; i++){
+        let scoreInq = Number($("#score" + player + "-" + i + "").html());
+        if(isNaN(scoreInq) === false) {
+            outScore =  scoreInq + outScore;
         }
     }
 
-    for (let i = 1; i <= golfHoles.length; i++){
-        let scoreCheck = $("#score" + player + "-" + i + "")[0].innerHTML;
-        if(!isNaN(Number(scoreCheck)) === true){
-            initScore = Number(scoreCheck) + initScore;
-        }
-    }
-    $("#total" + player + "")[0].innerHTML = initScore;
+    $("#inTotal"+ player +"").html(inScore);
+    $("#outTotal" + player + "").html(outScore);
+    $("#gameTotal" + player + "").html(inScore + outScore);
 
 }
 
